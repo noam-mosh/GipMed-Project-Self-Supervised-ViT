331e250d189babbfff122380cca6027e3e6daebc
Tom R <tomrahav500@gmail.com>
2023-01-04 08:05:40 +0200

    Update

--- py-sbatch.sh
+++ py-sbatch.sh
@@ -43,14 +43,6 @@ sbatch \
 #!/bin/bash
 echo "*** SLURM BATCH JOB '$JOB_NAME' STARTING ***"
 
-# Setup the conda env
-echo "*** Activating environment $CONDA_ENV ***"
-source $CONDA_HOME/etc/profile.d/conda.sh
-conda activate $CONDA_ENV
 
-# Run python with the args to the script
-python $@
-
-echo "*** SLURM BATCH JOB '$JOB_NAME' DONE ***"
 EOF
 
